fe0248de4cf4fde6272a9e002f1e18190a50416c
phiychai <charles.hyland.dev@gmail.com>
2025-02-18 17:19:20 +0100

    add types

---
+++ src/types/car.ts
@@ -0,0 +1,49 @@
+export type CarColors = {
+  colors: string[];
+};
+
+export type Model = {
+  name: string;
+};
+
+export type Manufacturer = {
+  name: string;
+  models: Model[];
+};
+
+export type ManufacturerCollection = {
+  manufacturers: Manufacturer[];
+};
+
+export type FuelType = 'Diesel' | 'Petrol';
+export type MileageUnit = 'km' | 'mi';
+
+export type Mileage = {
+  number: number;
+  unit: MileageUnit;
+};
+
+export type Car = {
+  stockNumber: number;
+  manufacturerName: string;
+  modelName: string;
+  color: string;
+  mileage: Mileage;
+  fuelType: FuelType;
+  pictureUrl: string;
+};
+
+export type CarsCollectionParams = {
+  color?: string;
+  page: number;
+  manufacturer?: string;
+};
+
+export type CarsCollectionPaginated = {
+  cars: Car[];
+  totalPageCount: number;
+  totalCarsCount: number;
+}
+
+
+
